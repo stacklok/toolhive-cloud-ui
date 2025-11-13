41074fcb55a871e13b3af4873ddb78c7c21fab93
Daniel Kantor <kd@stacklok.com>
2025-11-13 12:48:49 +0100

    style: reformat files to make biome happy

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,10 +1,13 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import Home from '../src/app/page'
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
+import Home from "../src/app/page";
 
-test('Home page', () => {
-  render(<Home />)
+test("Home page", () => {
+  render(<Home />);
   expect(
-    screen.getByRole('heading', { level: 1, name: /To get started, edit the page.tsx file./i })
-  ).toBeDefined()
-})
+    screen.getByRole("heading", {
+      level: 1,
+      name: /To get started, edit the page.tsx file./i,
+    }),
+  ).toBeDefined();
+});
