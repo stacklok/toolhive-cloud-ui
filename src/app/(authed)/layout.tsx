f86ec1edcb82ce2e35566e75740fc0994d02ca9b
Daniel Kantor <kd@stacklok.com>
2025-11-21 10:50:28 +0100

    leverage route groups to group logged-in pages together

---
+++ src/app/(authed)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components/navbar";
+
+export default async function AuthedLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
-import { Navbar } from "@/components/navbar";
 import "./globals.css";
 
 const inter = Inter({
@@ -22,7 +21,6 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
-        <Navbar />
         {children}
         <Toaster
           richColors
